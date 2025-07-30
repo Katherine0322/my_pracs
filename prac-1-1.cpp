622fd13f288b891f582f0472a9a24412eaf1f84e
Katherine0322 <3031760050@qq.com>
2025-07-30 16:32:59 +0930

    my first files to practial1

---
+++ prac-1-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+// Sums together all  numbers and returns them
+int sum_array(int array[], int n) {
+    int total = 0;
+    if (n<1)
+    {
+        return 0;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        total = total+array[i];
+    }
+
+    return total;
+}
